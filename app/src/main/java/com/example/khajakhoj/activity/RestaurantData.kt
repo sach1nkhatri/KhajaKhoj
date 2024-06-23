c58255309fdeaf1cb4841e6569405963ba643ab1
binodlamiochhane31 <lamibinod31@gmail.com>
2024-06-23 07:50:14 +0545

    resolved commits

--- app/src/main/java/com/example/khajakhoj/activity/RestaurantData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.khajakhoj.activity
-
-data class RestaurantData(
-    val titleImage: Int, // Assuming titleImage is a drawable resource ID
-    val restaurantTitle: String,
-    val restaurantCuisine: String
-)
