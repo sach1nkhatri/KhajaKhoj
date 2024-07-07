c275204fcd6bd28236f71a1531376a653ee614eb
binodlamiochhane31 <lamibinod31@gmail.com>
2024-07-07 14:27:50 +0545

    Finished maps direction

--- app/src/main/java/com/example/khajakhoj/utils/MapUtils.kt
+++ app/src/main/java/com/example/khajakhoj/utils/MapUtils.kt
@@ -2,18 +2,16 @@ package com.example.khajakhoj.utils
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.Window
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat
 import com.example.khajakhoj.R
 
 object MapUtils {
-    fun showMapsWebViewDialog(context: Context,mapUrl:String) {
+    fun showMapsWebViewDialog(context: Context, mapUrl: String) {
         val dialog = Dialog(context)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setCancelable(true)
@@ -21,19 +19,23 @@ object MapUtils {
         dialog.window?.setBackgroundDrawableResource(R.drawable.custom_dialog_background)
 
         val webView = dialog.findViewById<WebView>(R.id.webView)
-        webView.webViewClient = WebViewClient()
 
-        // this will load the url of the website
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let {
+                    if (it.startsWith("http://") || it.startsWith("https://")) {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                        context.startActivity(intent)
+                        return true
+                    }
+                }
+                return false
+            }
+        }
         webView.loadUrl(mapUrl)
-
-        // this will enable the javascript settings, it can also allow xss vulnerabilities
         webView.settings.javaScriptEnabled = true
-
-        // if you want to enable zoom feature
         webView.settings.setSupportZoom(true)
         webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-
-
 
         dialog.show()
     }
