41466d2aac1277e38957adc151c9334b08a55f80
sachin khatri <sachin10392313@gmail.com>
2024-07-10 00:38:16 +0545

    global search bar view initilization and filter search implementation and bookmark ui

--- app/src/main/java/com/example/khajakhoj/activity/GlobalSearchView.kt
+++ app/src/main/java/com/example/khajakhoj/activity/GlobalSearchView.kt
@@ -10,11 +10,6 @@ class GlobalSearchView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_global_search_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
